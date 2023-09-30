190c8466eb419e9bc2cd8e85526865f2915e4039
Yeseo <chinkl0110@naver.com>
2023-09-30 23:58:18 +0900

    main 컨트롤러

---
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class MainController {
+
+    @GetMapping("/sbb")
+    @ResponseBody
+    public String index() {
+        return "index";
+    }
+}
